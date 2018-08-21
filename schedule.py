71c695f8fb3b4171300c0de3e17c7ac5a9eefa0e
WeaverDyl <dweaver@umd.edu>
2018-08-21 19:56:43 -0400

    add more checks, base further completed

--- schedule.py
+++ schedule.py
@@ -3,19 +3,19 @@ import json
 import sys
 import getopt
 from time import sleep
-from datetime import date, datetime
+from datetime import date, datetime, timedelta
 
 def main():
     try:
-        args = semester, course, section = get_args() # Grab command-line args    
+        args = semester, year, course, section = get_args() # Grab command-line args    
         (result, message) = validate_args(args)
         if result:
-            formatted_semester = format_semester(semester)
+            formatted_semester = format_semester(semester, year)
             check_class(course, section, formatted_semester)
             return
         else:
             raise ValueError(message)
-    except (ValueError, getopt.GetoptError) as e: # specify
+    except Exception as e: # specify
         print(f"Error: {e}")
         usage()
         return None
@@ -32,10 +32,14 @@ def check_class(course, section, formatted_semester):
             site = resp.read()
             encode = resp.headers.get_content_charset('utf-8')
             site = site.decode(encode)
-            site = "".join(site.split())
+            site = "".join(site.split()) # Remove any whitespace
 
         try:
             # TODO: use umd.io when it's not broken
+
+            # Check for misformed course ID / section number
+            if "No courses matched your search filters above.".replace(" ", "") in site:
+                raise ValueError("Invalid course or section")
 
             # Find section within HTML
             find_section = site.find(section_code)
@@ -53,37 +57,66 @@ def check_class(course, section, formatted_semester):
             # Else, sleep for 5 minutes and try again.
             print(f"No seats at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}. Checking in 5 minutes...")
             sleep(300)
+        except ValueError as e:
+            # Except in main()
+            raise
         except Exception as e: # Gather possibilities
-            # Can happen if section doesn't exist
             print("Unexpected error occurred. Exiting...")
             print(e)
             return
 
-def format_semester(semester):
+def format_semester(semester, year):
     # Returns the semester in the format that the schedule of classes expects:
     # semesterYY
     semester = semester.lower()
-    year = date.today().strftime("%y")
     # Returns the current semester based on the current date
     if semester in ("fall", "winter", "spring", "summer"):
         if semester == "fall":
-            return "08" + year
+            month = "08"
         elif semester == "winter":
-            return "12" + year
+            month = "12"
+            year = str(int(year) - 1)
         elif semester == "spring":
-            return "01" + year
+            month = "01"
         else:
-            return "05" + year
+            month = "05" # Summer
     else:
-        raise ValueError("You've entered an invalid semester!")
+        raise ValueError("That semester is invalid")
+
+    # Check that the semester hasn't already happened
+    if validate_semester(month, year):
+        return month + year
+    else:
+        raise ValueError("That semester is too far from the current date.")
+
+def validate_semester(month, year):
+    # Checks if the date provided is close to the dates given by the schedule
+    # of classes formatted month/year (1217, 0518, 0818, 0119)
+
+    # This is a pretty good way to ensure that no previous semesters are accidentally
+    # used.
+
+    # TODO: reconsider for some circumstances: summer I / summer II / winter length
+    try:
+        date_given = datetime.strptime(month + year, '%m%y')
+        date_buffer_greater = date_given + timedelta(days=45)
+        date_buffer_less = datetime.now() + timedelta(days=45)
+
+        # Check that we're within 45 days of the semester the user gave
+        # The 45 is arbitrary, but should work well enough for general use
+        if datetime.now() > date_buffer_greater or date_given > date_buffer_less:
+            return False
+        return True
+    except Exception:
+        raise
 
 def validate_args(args):
     # Checks to see if the required arguments were received
-    term, course, section = args
+    term, year, course, section = args
 
     # Must specify a specific semester (fall, spring, winter, summer)
-    if not term:
-        return (False, "You must specify a semester")
+    if not term or not year:
+        return (False, "You must specify a semester and year")
     # Must specify a course
     if not course:
         return (False, "You must specify a course")
@@ -103,7 +136,8 @@ def get_args():
     # Set boolean flags for each short/longopt
     for opt, arg in opts:
         if opt in ("-t", "--term"):
-            term = arg
+            term = arg[:len(arg) - 2]
+            year = arg[len(arg) - 2:]
         elif opt in ("-c", "--course"):
             course = arg
         elif opt in ("-s", "--section"):
@@ -113,14 +147,17 @@ def get_args():
             sys.exit(2)
         else:
             raise ValueError("You've entered an invalid argument.")
-    return (term, course, section)
+    return (term, year, course, section)
 
 def usage():
     # Print the directions to use the program
     print(f"\nusage: {sys.argv[0]} -t TERM -c COURSE -s SECTION")
-    print("Further Explanation:\n-t / --term\t: refers to the current semester. Valid input is:\n\t\t  Fall | Spring | Winter | Summer")
-    print("-c / --course\t: refers to the course department and number. An example is 'ENGL101'.\n\t\t  Courses are 4 letters followed by 3 numbers.")
-    print("-s / --section\t: refers to the section of the course. Sections are four digits, such as '0101'.")
+    print("Further Explanation:\n-t / --term\t: refers to the current semester and year."\
+          "Valid input is:\n\t\t  Fall## | Spring## | Winter## | Summer## (ex. fall18)")
+    print("-c / --course\t: refers to the course department and number. An example is 'ENGL101'."\
+          "\n\t\t  Courses are 4 letters followed by 3 numbers.")
+    print("-s / --section\t: refers to the section of the course. Sections are four digits, such "\
+          "as '0101'.")
 
 if __name__ == "__main__":
     main()

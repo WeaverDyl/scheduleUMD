77a0829ad48d989aa2bebc276bfd83118ee093c7
WeaverDyl <dweaver@umd.edu>
2018-08-21 11:53:15 -0400

    all except checking seats finished

--- schedule.py
+++ schedule.py
@@ -1,4 +1,4 @@
-import urllib.request
+from urllib.request import Request, urlopen
 import json
 import sys
 import getopt
@@ -10,18 +10,16 @@ def main():
         (result, message) = validate_args(args)
         if result:
             formatted_semester = format_semester(semester)
-            find_class(course, section, formatted_semester)
-            pass # THEN??
-            # ^ class = find_class(args) --- might be a list??? multiple sections may appear
-            # give user numbered list, ask them to choose their ideal section if applicable
-            # ask user for email/phone#
+            check_class(course, section, formatted_semester)
+            return
         else:
             raise ValueError(message)
-    except Exception as e: # specify
+    except (ValueError, getopt.GetoptError) as e: # specify
         print(f"Error: {e}")
+        usage()
         return None
 
-def find_class(course, section, formatted_semester):
+def check_class(course, section, formatted_semester):
     base_url = "https://app.testudo.umd.edu/soc/search?courseId={course}&sectionId={section}&termId=201808&_openSectionsOnly=on&creditCompare=&credits=&courseLevelFilter=ALL&instructor=&_facetoface=on&_blended=on&_online=on&courseStartCompare=&courseStartHour=&courseStartMin=&courseStartAM=&courseEndHour=&courseEndMin=&courseEndAM=&teachingCenter=ALL&_classDay1=on&_classDay2=on&_classDay3=on&_classDay4=on&_classDay5=on"
     pass
 
@@ -41,8 +39,7 @@ def format_semester(semester):
         else:
             return "05" + year
     else:
-        raise ValueError("You've entered an invalid semester! Run program with flag --help to see all options.")
-    pass
+        raise ValueError("You've entered an invalid semester!")
 
 def validate_args(args):
     # Checks to see if the required arguments were received
@@ -64,23 +61,30 @@ def get_args():
         term, course, section = None, None, None
         #course_set, section_set, begin_set, end_set, days_set = False, False, False, False, False
         opts, _ = getopt.getopt(sys.argv[1:], "t:c:s:h", ["term=", "course=", "section=", "help"])                           
-
-        # Set boolean flags for each short/longopt
-        for opt, arg in opts:
-            if opt in ("-t", "--term"):
-                term = arg
-            elif opt in ("-c", "--course"):
-                course = arg
-            elif opt in ("-s", "--section"):
-                section = arg
-            elif opt in ("-h", "--help"):
-                # Give user list of commands
-                pass
-            else:
-                raise ValueError("You've entered an invalid argument. Run program with flag --help to see all options.")
-        return (term, course, section)
     except getopt.GetoptError as e:
         raise ValueError(e) from None
+
+    # Set boolean flags for each short/longopt
+    for opt, arg in opts:
+        if opt in ("-t", "--term"):
+            term = arg
+        elif opt in ("-c", "--course"):
+            course = arg
+        elif opt in ("-s", "--section"):
+            section = arg
+        elif opt in ("-h", "--help"):
+            usage()
+            sys.exit(2)
+        else:
+            raise ValueError("You've entered an invalid argument.")
+    return (term, course, section)
+
+def usage():
+    # Print the directions to use the program
+    print(f"\nusage: {sys.argv[0]} -t TERM -c COURSE -s SECTION")
+    print("Further Explanation:\n-t / --term\t: refers to the current semester. Valid input is:\n\t\t  Fall | Spring | Winter | Summer")
+    print("-c / --course\t: refers to the course department and number. An example is 'ENGL101'.\n\t\t  Courses are 4 letters followed by 3 numbers.")
+    print("-s / --section\t: refers to the section of the course. Sections are four digits, such as '0101'.")
 
 if __name__ == "__main__":
     main()
